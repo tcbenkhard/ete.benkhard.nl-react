cc3d37ede265a043ec9163fbb8968bf15836eecc
Timo Benkhard <tcbenkhard@gmail.com>
2024-02-15 08:59:45 +0100

    Fix useEfffect

--- src/pages/OverviewPage.tsx
+++ src/pages/OverviewPage.tsx
@@ -20,19 +20,17 @@ export const OverviewPage = () => {
     const [meals, setMeals] = useState<Array<Meal>>([])
 
     useEffect(() => {
-        return () => {
-            setMeals([
-                {id: 'spaghettibolognese', description: 'Fruit de uitjes en bak daarna de gehakt bruin. Voeg hierna de tomatenpuree toe en laat het even inbakken. Net voor het einde wat pastawater toevoegen om de saus te maken.', name: 'Spaghetti Bolognese', tags: ['pasta', 'italiaans'], ingredients: ['1 ui', 'pasta', '300gr rundergehakt', '1 blikje tomatenpuree']},
-                {id: 'kipkerrie', name: 'Kip kerrie', tags: ['pasta', 'italiaans'], ingredients: ['1 ui', 'pasta', '300gr rundergehakt', '1 blikje tomatenpuree']},
-                {id: 'carbonara', name: 'Carbonara', tags: ['pasta', 'italiaans'], ingredients: ['1 ui', 'pasta', '300gr rundergehakt', '1 blikje tomatenpuree']},
-                {id: 'boerenkool', name: 'Boerenkool', tags: ['pasta', 'italiaans'], ingredients: ['1 ui', 'pasta', '300gr rundergehakt', '1 blikje tomatenpuree']},
-                {id: 'groentesoep', name: 'Groentesoep', tags: ['pasta', 'italiaans'], ingredients: ['1 ui', 'pasta', '300gr rundergehakt', '1 blikje tomatenpuree']},
-                {id: 'kerriesoep', name: 'Kerriesoep', tags: ['pasta', 'italiaans'], ingredients: ['1 ui', 'pasta', '300gr rundergehakt', '1 blikje tomatenpuree']},
-                {id: 'wrapskip', name: 'Wraps met kip', tags: ['pasta', 'italiaans'], ingredients: ['1 ui', 'pasta', '300gr rundergehakt', '1 blikje tomatenpuree']},
-                {id: 'nachosgehakt', name: 'Nachos met gehakt', tags: ['pasta', 'italiaans'], ingredients: ['1 ui', 'pasta', '300gr rundergehakt', '1 blikje tomatenpuree']},
-                {id: 'vitellotonato', name: 'Vitello Tonato', tags: ['pasta', 'italiaans'], ingredients: ['1 ui', 'pasta', '300gr rundergehakt', '1 blikje tomatenpuree']},
-            ])
-        };
+        setMeals([
+            {id: 'spaghettibolognese', description: 'Fruit de uitjes en bak daarna de gehakt bruin. Voeg hierna de tomatenpuree toe en laat het even inbakken. Net voor het einde wat pastawater toevoegen om de saus te maken.', name: 'Spaghetti Bolognese', tags: ['pasta', 'italiaans'], ingredients: ['1 ui', 'pasta', '300gr rundergehakt', '1 blikje tomatenpuree']},
+            {id: 'kipkerrie', name: 'Kip kerrie', tags: ['pasta', 'italiaans'], ingredients: ['1 ui', 'pasta', '300gr rundergehakt', '1 blikje tomatenpuree']},
+            {id: 'carbonara', name: 'Carbonara', tags: ['pasta', 'italiaans'], ingredients: ['1 ui', 'pasta', '300gr rundergehakt', '1 blikje tomatenpuree']},
+            {id: 'boerenkool', name: 'Boerenkool', tags: ['pasta', 'italiaans'], ingredients: ['1 ui', 'pasta', '300gr rundergehakt', '1 blikje tomatenpuree']},
+            {id: 'groentesoep', name: 'Groentesoep', tags: ['pasta', 'italiaans'], ingredients: ['1 ui', 'pasta', '300gr rundergehakt', '1 blikje tomatenpuree']},
+            {id: 'kerriesoep', name: 'Kerriesoep', tags: ['pasta', 'italiaans'], ingredients: ['1 ui', 'pasta', '300gr rundergehakt', '1 blikje tomatenpuree']},
+            {id: 'wrapskip', name: 'Wraps met kip', tags: ['pasta', 'italiaans'], ingredients: ['1 ui', 'pasta', '300gr rundergehakt', '1 blikje tomatenpuree']},
+            {id: 'nachosgehakt', name: 'Nachos met gehakt', tags: ['pasta', 'italiaans'], ingredients: ['1 ui', 'pasta', '300gr rundergehakt', '1 blikje tomatenpuree']},
+            {id: 'vitellotonato', name: 'Vitello Tonato', tags: ['pasta', 'italiaans'], ingredients: ['1 ui', 'pasta', '300gr rundergehakt', '1 blikje tomatenpuree']},
+        ])
     }, []);
 
 
