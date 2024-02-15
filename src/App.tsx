001df38852ae61ecd4d2a3b684747fff5876ec23
Timo Benkhard <tcbenkhard@gmail.com>
2024-02-15 13:35:43 +0100

    Set title

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.scss';
 import "@fontsource/roboto";
 import {OverviewPage} from "./pages/OverviewPage";
 
 const App = () => {
+    useEffect(() => {
+        document.title = 'Eten - Benkhard.com';
+    }, []);
+
   return (
     <div className="App">
       <OverviewPage />
